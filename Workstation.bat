ea2e3514067d3dc3b103c161220b685f0a824e92
Kyle Sanders <63331106+KSanders7070@users.noreply.github.com>
2023-09-19 15:37:26 -0700

    Add files via upload

--- Workstation.bat
+++ Workstation.bat
@@ -27,20 +27,30 @@ setlocal enabledelayedexpansion
 	cls
 	
 	set "BatchAppDataDir=%LocalAppdata%\WorkstationBatch"
-	if not exist "!BatchAppDataDir!" md "!BatchAppDataDir!"
+	if not exist "!BatchAppDataDir!" call :InitialSetup
+	
 	
 	cd /d "!BatchAppDataDir!"
 
 
 
+:CallFunctions
 
+:InitialSetup
+	
+	:: Creates a directory for this BATCH file to host data in like the config file and preferences.
+	md "!BatchAppDataDir!"
+	
+	:: Creates a Config file for the user to be able to edit.
+	(
+		echo Common Name,Type (program/website),PATH,URL
+		echo GOOGLE SITE,WEBSITE,,WWW.GOOGLE.COM
+		echo NOAA SITE,WEBSITE,,WWW.NOAA.GOV
+		echo vATIS,PROGRAM,%LocalAppdata%\vATIS-4.0\Application\vATIS.exe,
+		echo AFV,PROGRAM,C:\AudioForVATSIM\AudioForVATSIM.exe,
+		echo CRC,PROGRAM,%LocalAppdata%\CRC\Application\CRC.exe --debug,
 
-
-
-
-
-
-
+	)>Workstation_Config.csv
 
 
 
